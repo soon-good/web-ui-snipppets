18f828a42e8ca8b8b857e8a569696dbe99ce7be5
soon.good <playmymind@daum.net>
2022-06-30 23:43:03 +0900

    검색어 자동완성 기능 추가

---
+++ snippets/auto-complete/js/main.js
@@ -0,0 +1,36 @@
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+const searchStates = async searchText => {
+	const res = await fetch('../data/state_capitals.json');
+	const states = await res.json();
+
+	let matches = states.filter(state => {
+		// gi : 'g' = global, 'i' = case insensitive 
+		const regex = new RegExp(`^${searchText}`, 'gi');
+		return state.name.match(regex) || state.abbr.match(regex);
+	});
+
+	if(searchText.length === 0){
+		matches = [];
+	}
+
+	renderHtml(matches);
+}
+
+const renderHtml = matches => {
+	if(matches.length > 0){
+		const html = matches.map(match => `
+			<div class="card card-body mb-1">
+				<h4>${match.name} (${match.abbr})
+					<span class="text-primary"> ${match.capital} </span>
+				</h4>
+				<small> Lat: ${match.lat} / Long: ${match.long} </small>
+			</div>
+		`).join('');
+
+		matchList.innerHTML = html;
+	}
+}
+
+search.addEventListener('input', () => searchStates(search.value));
