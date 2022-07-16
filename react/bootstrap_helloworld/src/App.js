af34d2f3ce67e6d48ad79f3081b951c3508cdd99
soon.good <playmymind@daum.net>
2022-07-16 13:18:43 +0900

    (frontend) 부트스트랩 테마 적용
    = 로딩페이지와 네비게이션 헤더 목업 추가 및 컴포넌트로 분리

--- react/bootstrap_helloworld/src/App.js
+++ react/bootstrap_helloworld/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { Landing } from './Landing';
+import { Navbar } from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Landing/>
     </div>
   );
 }
