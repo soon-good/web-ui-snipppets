fdadedc3925ca4d1793a343d0a99d16548d0c93b
soon.good <playmymind@daum.net>
2022-07-18 21:31:36 +0900

    (react/react-router-dom) 기본 라우터 틀 구현

--- react/react_bootstrap_hello1/src/App.js
+++ react/react_bootstrap_hello1/src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './screens/Home';
+import About from './screens/About';
+import Projects from './screens/Projects';
+import Questions from './screens/Questions';
+import ReactDoc from './screens/ReactDoc';
+import NavBarElements from './components/NavBarElements';
 
 function App() {
   return (
-    <div className="App">
-      <h1>테스트!!!</h1>
-    </div>
+    <Router>
+      <NavBarElements/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Projects" element={<Projects/>}/>
+        <Route path="/Questions" element={<Questions/>}/>
+        <Route path="/ReactDoc" element={<ReactDoc/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ react/react_bootstrap_hello1/src/screens/ReactDoc.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReactDoc = () => {
+  return (
+	<div>ReactDoc</div>
+  )
+}
+
+export default ReactDoc;
